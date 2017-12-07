8f48a341a1f966e6520de6ce3e4971a46b27594f
katiescruggs <katie.e.scruggs@gmail.com>
2017-12-07 08:21:16 -0700

    add star icons to cards to display favorites

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './Card.css';
+import StarIcon from 'react-icons/lib/fa/star';
+import OpenStarIcon from 'react-icons/lib/fa/star-o';
+
 
 const Card = ({fav, category, addFav, removeFav, name, stats}) => {
 
   const type = fav ? 'card fav' : 'card';
+  const star = fav ? <StarIcon className="fav-star-icon"/> : <OpenStarIcon className="fav-star-icon open"/>;
   
   const addRemoveFav = () => {
     fav ? removeFav(name, category) : addFav(name, category);
@@ -18,6 +22,7 @@ const Card = ({fav, category, addFav, removeFav, name, stats}) => {
     <div className={`${type} ${category}`} onClick={addRemoveFav}>
       <header>
         <h3 className="card-title">{name}</h3>
+        {star}
       </header>
       <ul className="card-details">
         {statElements}
