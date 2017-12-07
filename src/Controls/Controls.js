c93615995c74434b38f3afa012d525717c1d1500
katiescruggs <katie.e.scruggs@gmail.com>
2017-12-07 08:03:44 -0700

    add hover states and focus states to controls buttons

--- src/Controls/Controls.js
+++ src/Controls/Controls.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import './Controls.css';
+import MaleIcon from 'react-icons/lib/fa/male';
+import GlobeIcon from 'react-icons/lib/fa/globe';
+import AutoIcon from 'react-icons/lib/fa/automobile';
+import StarIcon from 'react-icons/lib/fa/star';
+
+
 
 const Controls = ({chooseCategory, favorites}) => {
   const favNum = favorites.length;
   return (
     <div className="control-buttons">
-      <button onClick={chooseCategory}>people</button>
-      <button onClick={chooseCategory}>planets</button>
-      <button onClick={chooseCategory}>vehicles</button>
-      <button onClick={() => chooseCategory({target: {innerText: 'favorites'}}) }>favorites - {favNum}</button>
+      <button onClick={chooseCategory}>
+        <MaleIcon className="icon"/>
+        people
+      </button>
+
+      <button onClick={chooseCategory}>
+        <GlobeIcon className="icon"/>
+        planets
+      </button>
+
+      <button onClick={chooseCategory}>
+        <AutoIcon className="icon"/>
+        vehicles
+      </button>
+
+      <button
+        className="fav-button" 
+        onClick={() => chooseCategory({target: {innerText: 'favorites'}}) }
+      >
+        <StarIcon className="icon"/>
+        favorites
+        <span className="fav-num">{favNum}</span>
+      </button>
     </div>
   );
 };
