4d18d24e1d1d6177df150551b49fd0aebc479c48
katiescruggs <katie.e.scruggs@gmail.com>
2017-12-09 13:20:12 -0700

    add active class to button whose category is active

--- src/Controls/Controls.js
+++ src/Controls/Controls.js
@@ -6,25 +6,42 @@ import AutoIcon from 'react-icons/lib/fa/automobile';
 import StarIcon from 'react-icons/lib/fa/star';
 import PropTypes from 'prop-types';
 
+const buttonInfo = [
+  {
+    className: 'people-button',
+    text: 'people',
+    icon: <MaleIcon className="icon"/>,
+  },
+  {
+    className: 'planets-button',
+    text: 'planets',
+    icon: <GlobeIcon className="icon"/>
+  },
+  {
+    className: 'vehicles-button',
+    text: 'vehicles',
+    icon: <AutoIcon className="icon"/>
+  }
+]
 
-const Controls = ({chooseCategory, favorites}) => {
+
+const Controls = ({category, chooseCategory, favorites}) => {
+
+  const buttons = buttonInfo.map(button => {
+    let className = category === button.text ? `${button.className} active`: button.className;
+
+    return (
+      <button className={className} onClick={chooseCategory}>
+        {button.icon}
+        {button.text}
+      </button>
+    );
+  });
   const favNum = favorites.length;
+  
   return (
     <div className="control-buttons">
-      <button className="people-button" onClick={chooseCategory}>
-        <MaleIcon className="icon"/>
-        people
-      </button>
-
-      <button className="planets-button" onClick={chooseCategory}>
-        <GlobeIcon className="icon"/>
-        planets
-      </button>
-
-      <button className="vehicles-button" onClick={chooseCategory}>
-        <AutoIcon className="icon"/>
-        vehicles
-      </button>
+      {buttons}
 
       <button
         className="fav-button" 
