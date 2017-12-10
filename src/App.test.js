d1c1509e49ad60f8b2d93114e4957b44d3897ee7
katiescruggs <katie.e.scruggs@gmail.com>
2017-12-10 15:21:50 -0700

    mock localStorage in App test

--- src/App.test.js
+++ src/App.test.js
@@ -2,13 +2,15 @@ import React from 'react';
 import App from './App';
 import {shallow} from 'enzyme';
 
-// window.fetch = jest.fn().mockImplementation(() => {
-//   Promise.resolve({
-//     json: () => Promise.resolve({
-//       results: [{1: 'object1'}, {2: 'object2'}]
-//     });
-//   });
-// });
+global.localStorage = {
+  result: {},
+  setItem: (key, value) => {
+    localStorage.result[key] = value;
+  },
+  getItem: (key) => {
+    return localStorage.result[key];
+  }
+}
 
 describe('App', () => {
   it('should render correctly', () => {
